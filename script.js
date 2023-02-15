a14872db61bbae7b03e0d5dfc8acccd1baa4eb59
Nurpolat Yerkinbayev <nurbilim.kz@gmail.com>
2023-02-15 19:04:19 +0600

    Modifies 'script.js':     Modifies 'addingClickEventsToEmptyCells' function.     Defines 'markX' and 'markO' functions.     Makes each cell in the game board get properly marked with the     right marks ("X" or "O").

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
 let gameboard = (function () {
-    
-    addingClickEventsToCells("X");
+    let emptyCells = document.querySelectorAll(".empty-cell");
+    addingClickEventsToEmptyCells(emptyCells);
 })();
 
-function addingClickEventsToCells(symbolText) { //symbolText = "X" or symbolText = "O"
-    let cells = document.querySelectorAll(".cell");
-    cells.forEach((cell) => {
-        cell.addEventListener('click', (e) => {
-            let c = e.target;
-            let symbol = document.createElement('h1');
-            symbol.textContent = symbolText;
-            c.appendChild(symbol);
-        }, {once: true});
+function addingClickEventsToEmptyCells(emptyCells) {
+    emptyCells.forEach((cell) => {
+        cell.addEventListener('click', markX, {once: true});
     });
 }
 
+function markX(e) {
+    let symbol = document.createElement('h1');
+    symbol.textContent = "X";
+    this.appendChild(symbol);
+    this.classList.remove("empty-cell");
+
+    let allCells = document.querySelectorAll(".cell");
+    allCells.forEach((cell) => {
+        cell.removeEventListener('click', markX);
+    });
+
+    let remainingEmptyCells = document.querySelectorAll(".empty-cell");
+
+    remainingEmptyCells.forEach((cell) => {
+        cell.addEventListener('click', markO, {once: true});
+    })
+}
+
+function markO() {
+    let symbol = document.createElement('h1');
+    symbol.textContent = "O";
+    this.appendChild(symbol);
+    this.classList.remove("empty-cell");
+
+    let allCells = document.querySelectorAll(".cell");
+    allCells.forEach((cell) => {
+        cell.removeEventListener('click', markO);
+    });
+
+    let remainingEmptyCells = document.querySelectorAll(".empty-cell");
+
+    remainingEmptyCells.forEach((cell) => {
+        cell.addEventListener('click', markX, {once: true});
+    })
+}
